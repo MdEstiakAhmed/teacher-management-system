0ba45b08b19662b95479b2f7bb806d6f6f002992
Md. Estiak Ahmed <17-33434-1@student.aiub.edu>
2023-01-23 22:24:04 +0600

    fix graph issue

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import AllTeacherListAnalytics from "../components/pages/dashboard/AllTeacherListAnalytics";
 import EventListAnalytics from "../components/pages/dashboard/EventListAnalytics";
 import MeAsSupervisorListAnalytics from "../components/pages/dashboard/MeAsSupervisorListAnalytics";
@@ -8,34 +8,36 @@ import UserListAnalytics from "../components/pages/dashboard/UserListAnalytics";
 const Dashboard = () => {
     const containerRef = useRef(null);
 
+    const [isDataShow, setIsDataShow] = useState();
+
     useEffect(() => {
-        if (containerRef) {
-            console.log(containerRef.current.clientWidth);
+        if (containerRef?.current?.clientWidth) {
+            setIsDataShow(containerRef.current.clientWidth)
         }
     }, [containerRef]);
     return (
         <>
             <div className="contentArea analyticsArea" ref={containerRef}>
                 {
-                    containerRef?.current?.clientWidth ? (
+                    isDataShow ? (
                         <div className="analyticsArea-wrapper">
                             <div className="analyticsArea-group1">
                                 <UserListAnalytics
-                                    width={containerRef?.current?.clientWidth / 4 || 0}
+                                    width={containerRef.current.clientWidth / 4 || 0}
                                 />
                                 <MyTaskListAnalytics
-                                    width={containerRef?.current?.clientWidth / 4 || 0}
+                                    width={containerRef.current.clientWidth / 4 || 0}
                                 />
                                 <MeAsSupervisorListAnalytics
-                                    width={containerRef?.current?.clientWidth / 4 || 0}
+                                    width={containerRef.current.clientWidth / 4 || 0}
                                 />
                                 <EventListAnalytics
-                                    width={containerRef?.current?.clientWidth / 4 || 0}
+                                    width={containerRef.current.clientWidth / 4 || 0}
                                 />
                             </div>
                             <div className="analyticsArea-group2">
                                 <AllTeacherListAnalytics
-                                    width={containerRef?.current?.clientWidth / 4 || 0}
+                                    width={containerRef.current.clientWidth / 4 || 0}
                                 />
                             </div>
                         </div>
